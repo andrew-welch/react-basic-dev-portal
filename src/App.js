ec8bb948be615cb231776f01ee7b7d781c3aade1
Andrew Welch <19201714+andrew-welch@users.noreply.github.com>
2023-11-23 16:42:15 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>Hello {value}</div>;
+  return exists('https://google.com');
 }
 
 export default App;
+
+
+async function exists(url) {
+  const result = await fetch(url, { method: 'HEAD' });
+  return result.ok;
+  console.log (result);
+  
+}
